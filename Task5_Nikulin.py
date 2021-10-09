ad77e3570b73ad27c10333408354bb3f1d88b92d
ArtsemNikulin <mrhuku@mail.ru>
2021-10-09 19:04:58 +0300

    Updated

--- Task5_Nikulin.py
+++ Task5_Nikulin.py
@@ -15,22 +15,20 @@ class Choose:
 class Publication:
     def __init__(self):
         self.date = datetime.datetime.now()
-
         self.text_of_publication = input(f'Enter text of publication:\n')
+        self.content = ''
+
+    def write_to_file(self, target_of_writing="News.txt"):
+        with open(target_of_writing, "a") as news_feed:
+            news_feed.write(self.content)
 
 
 class News(Publication):
     def __init__(self):
         super().__init__()
         self.city = input('Enter city of news:\n')
-
-    def write_to_file(self, target_of_writing="News.txt"):
-        news_publ_date = f"{self.city}, {self.date.strftime('%d/%m/%y %I.%M')}"
-        target_of_writing = open(target_of_writing, "a")
-        target_of_writing.write(f"News------------------\n"
-                                f"{self.text_of_publication}\n"
-                                f"{news_publ_date}\n\n")
-        target_of_writing.close()
+        news_publ_date = f"{self.city}, {self.date.strftime('%d/%m/%Y %I.%M')}"
+        self.content = f"News------------------\n{self.text_of_publication}\n{news_publ_date}\n\n"
 
 
 class Ads(Publication):
@@ -40,15 +38,10 @@ class Ads(Publication):
         actual_month = int(input('Enter a month till that AD will be actual\n'))
         actual_day = int(input('Enter a day of chose month till that AD will be actual\n'))
         actual_date = datetime.date(actual_year, actual_month, actual_day)
-        self.ads_actual_date = f"{actual_date.day}/{actual_date.month}/{actual_date.year}"
+        self.ads_actual_date = f"{actual_date.strftime('%d/%m/%Y')}"
         self.days_until = (actual_date - self.date.date()).days
-
-    def write_to_file(self, target_of_writing="News.txt"):
-        target_of_writing = open(target_of_writing, "a")
-        target_of_writing.write(f"Private Ad------------\n"
-                                f"{self.text_of_publication}\n"
-                                f"Actual until: {self.ads_actual_date}, {self.days_until} days left\n\n")
-        target_of_writing.close()
+        self.content = f"Private Ad------------\n{self.text_of_publication}\nActual until:" \
+                       f"{self.ads_actual_date}, {self.days_until} days left\n\n"
 
 
 class HelloMessage(Publication):
@@ -56,33 +49,17 @@ class HelloMessage(Publication):
         self.user_name = input("Enter your name:\n")
         self.receiver = input("Enter a name of message receiver:\n")
         super().__init__()
-
-    def write_to_file(self, target_of_writing="News.txt"):
-        target_of_writing = open(target_of_writing, "a")
-        target_of_writing.write(f"Hello message---------\n"
-                                f"From {self.user_name} TO {self.receiver}\n"
-                                f"{self.text_of_publication}\n\n")
-        target_of_writing.close()
+        self.content = f"Hello message---------\nFrom {self.user_name} TO {self.receiver}\n" \
+                       f"{self.text_of_publication}\n\n"
 
 
 while True:
     choose_type = Choose()
-
     if choose_type.type_of_publication == '1':
-        new_news = News()
-        new_news.write_to_file()
-
+        News().write_to_file()
     elif choose_type.type_of_publication == '2':
-        new_ads = Ads()
-        new_ads.write_to_file()
-
+        Ads().write_to_file()
     elif choose_type.type_of_publication == '3':
-        new_hello_mesage = HelloMessage()
-        new_hello_mesage.write_to_file()
-
+        HelloMessage().write_to_file()
     elif choose_type.type_of_publication == '4':
         sys.exit()
-
-    else:
-        print('Enter digit from 1 to 3 or 4 for Exit')
-        choose_type = Choose()
